8585aea6880b2e5cf4a961baa53eb73b0fbc225e
Franco Lucchini <flucchini@gmail.com>
2022-01-28 08:10:48 +0100

    add packages installer

---
+++ arch-install.sh
@@ -0,0 +1,15 @@
+# kernel+drivers
+pacman -S linux linux-firmware acpi iwd
+
+# base system
+pacman -S systemd-sysvcompat arch-install-scripts efibootmgr sudo less openssh bash-completion
+
+# base utils
+pacman -S neovim mc ncdu git rsync
+
+# base network
+pacman -S iproute2 iputils inetutils
+
+# sway+firefox (optional)
+# pacman -S firefox sway waybar wofi alacritty xorg-xwayland
+
